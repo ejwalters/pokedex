5cb034d4479f9baae128dab3120d280a351d3964
ejwalters <ejwalters24@gmail.com>
2016-08-16 19:41:53 -0400

    Initial Commit

---
+++ pokedex-eric-walters/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-eric-walters
+//
+//  Created by Eric Walters on 8/16/16.
+//  Copyright © 2016 Eric Walters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
